b0ab1c6d5f533d49a5dbc3dabf7ab3afdafd6a52
raviganesh m <raviganeshmbhat999@gmail.com>
2022-05-25 23:00:05 +0530

    Added auth to route PATCH /tasks/:id , Added middleware to delete all tasks of a user when that user is deleted

--- src/routers/task.js
+++ src/routers/task.js
@@ -44,34 +44,41 @@ router.get('/tasks/:id',  auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth,  async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    const userId = req.user._id
 
     if(!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates'})
     }
 
     try {
-        const task = await Task.findById(req.params.id)
+        //const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: userId})
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
         updates.forEach((update) => {
             task[update] = req.body[update]
         })
         await task.save()
         //const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.status(200).send(task)
+
+        return res.status(200).send(task)
     } catch(e) {
-        res.status(400).send(e)
+       return res.status(400).send(e)
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        // const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
 
         if(!task) {
             return res.status(404).send()
